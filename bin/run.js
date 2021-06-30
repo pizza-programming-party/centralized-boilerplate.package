6db637ce2167907f562bb75969f1713f8b9eeb2f
david <davidispizza@gmail.com>
2021-06-30 22:31:07 +0200

    use experimental installs

--- bin/run.js
+++ bin/run.js
@@ -28,5 +28,53 @@ boiler.run(path.resolve(__dirname, '..'), [
     action: 'full-text-replace',
     source: [ 'npmignore' ],
     destination: [ '.npmignore' ]
+  },
+  {
+    action: 'full-text-replace',
+    source: [ 'gulpfile.ts' ],
+    destination: [ 'gulpfile.ts' ]
+  }
+])
+
+boiler.install([
+  {
+    name: '@types/gulp',
+    version: 'latest',
+    environment: 'development'
+  },
+  {
+    name: '@types/jasmine',
+    version: 'latest',
+    environment: 'development'
+  },
+  {
+    name: '@types/node',
+    version: 'latest',
+    environment: 'development'
+  },
+  {
+    name: 'gulp',
+    version: 'latest',
+    environment: 'development'
+  },
+  {
+    name: 'jasmine',
+    version: 'latest',
+    environment: 'development'
+  },
+  {
+    name: 'ts-node',
+    version: 'latest',
+    environment: 'development'
+  },
+  {
+    name: 'ts-standard',
+    version: 'latest',
+    environment: 'development'
+  },
+  {
+    name: 'typescript',
+    version: 'latest',
+    environment: 'development'
   }
 ])
